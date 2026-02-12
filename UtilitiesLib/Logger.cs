bef4b54f7ccb6d0562c39854b35f6adf6c3ca136
Bloodbone9829 <marcomatejic@gmail.com>
2026-02-12 21:56:29 +0100

    a

---
+++ UtilitiesLib/Logger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace UtilitiesLib
+{
+    public static class Logger
+    {
+        public static Action<string> WriteMessage;
+
+        // This method is used to log a message. It will call the WriteMessage action if it has been subscribed to.
+        public static void LogMessage(string msg)
+        {
+            WriteMessage?.Invoke(msg);            
+        }
+    }
+}
